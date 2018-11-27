e00d74ef95fecd6f95bd67a4c1e4fc4763649541
k2snowman69 <rickyp@snowcoders.com>
2018-11-26 22:00:37 -0500

    Fixed sorting of function types

--- src/cli/mocha-fix/index.test.ts
+++ src/cli/mocha-fix/index.test.ts
@@ -1,9 +1,11 @@
 import { expect } from "chai";
+
 import * as sinon from "sinon";
 
 import { run } from "../index";
 
 // Mocks
+import * as cosmiconfig from "cosmiconfig";
 import { Reprinter } from "../../reprinter";
 import { LoggerVerboseOption, LogUtils } from "../../utilities/log-utils";
 
@@ -43,5 +45,80 @@ describe("cli", () => {
 
     expect(logMock.lastCall).to.be.null;
     expect(reprinterMock.lastCall.args[0]).to.contain("/package.json");
+  });
+
+  it("Prints error message if rewrite fails", () => {
+    reprinterMock.throws("Some error");
+
+    run(["./package.json"]);
+
+    expect(logMock.lastCall.args[0]).to.equal(LoggerVerboseOption.Normal);
+    expect(logMock.lastCall.args[1]).to.contain("Some error");
+  });
+
+  // TODO Figure out how to stub cosmiconfig
+  xdescribe("Cosmiconfig settings", () => {
+    let setVerbosityMock: sinon.SinonStub;
+    let cosmiconfigMock: sinon.SinonStubbedInstance<any>;
+    let config: Object;
+
+    before(() => {
+      setVerbosityMock = sinon.stub(LogUtils, "setVerbosity");
+      cosmiconfigMock = sinon.stub(cosmiconfig);
+      cosmiconfigMock.returns({
+        searchSync: () => {
+          return {
+            config: config
+          };
+        }
+      });
+    });
+
+    afterEach(() => {
+      cosmiconfigMock.reset();
+    });
+
+    after(() => {
+      cosmiconfigMock.restore();
+    });
+
+    it("Sets log level to diagnostic when set in config", () => {
+      config = {
+        logLevel: "diagnostic"
+      };
+
+      run(["./package.json"]);
+
+      expect(setVerbosityMock.callCount).to.equal(1);
+      expect(reprinterMock.lastCall.args[0]).to.be(
+        LoggerVerboseOption.Diagnostic as any
+      );
+    });
+
+    it("Sets log level to quiet when set in config", () => {
+      config = {
+        logLevel: "quiet"
+      };
+
+      run(["./package.json"]);
+
+      expect(setVerbosityMock.callCount).to.equal(1);
+      expect(reprinterMock.lastCall.args[0]).to.be(
+        LoggerVerboseOption.Quiet as any
+      );
+    });
+
+    it("Sets log level to normal when invalid in config", () => {
+      config = {
+        logLevel: "asdfasdf"
+      };
+
+      run(["./package.json"]);
+
+      expect(setVerbosityMock.callCount).to.equal(1);
+      expect(reprinterMock.lastCall.args[0]).to.be(
+        LoggerVerboseOption.Normal as any
+      );
+    });
   });
 });
---
+++ src/language-js/sortExpression/index.test.ts
@@ -0,0 +1,118 @@
+import { expect } from "chai";
+import { readFileSync } from "fs";
+import { sync } from "globby";
+import { basename, join } from "path";
+
+// Parsers
+import { parse as flowParse } from "../parsers/flow";
+import { parse as typescriptParse } from "../parsers/typescript";
+
+// The methods being tested here
+import { sortExpression } from "./index";
+
+// Utilities
+import { StringUtils } from "../../utilities/string-utils";
+
+interface TestInfo {
+  inputFilePath: string;
+  outputFilePath: string;
+  parserType: string;
+  testName: string;
+}
+
+describe("language-js/sortJsxElement", () => {
+  let parserTypes: string[];
+  let testInfos: TestInfo[];
+
+  parserTypes = [];
+
+  let assetsFolderPath = join(__dirname, "test_assets/*.input.txt");
+  testInfos = sync(assetsFolderPath).map(filePath => {
+    let segments = basename(filePath).split(".");
+
+    if (parserTypes.indexOf(segments[0]) === -1) {
+      parserTypes.push(segments[0]);
+    }
+
+    let cleanedTestName = StringUtils.sentenceCase(
+      segments[1].replace(/_/g, " ")
+    );
+
+    return {
+      inputFilePath: filePath,
+      outputFilePath: filePath.replace(".input.txt", ".output.txt"),
+      parserType: segments[0],
+      testName: cleanedTestName
+    };
+  });
+
+  parserTypes.forEach(fileType => {
+    describe(fileType, () => {
+      let parser;
+      switch (fileType) {
+        case "es6":
+        case "flow":
+          parser = flowParse;
+          break;
+        case "typescript":
+          parser = typescriptParse;
+          break;
+        default:
+          throw new Error(
+            "Unknown parser passed - " +
+              fileType +
+              ". Expected 'flow', 'typescript' or 'es6'."
+          );
+      }
+
+      testInfos.forEach(testInfo => {
+        if (testInfo.parserType == fileType) {
+          it(testInfo.testName, () => {
+            let input = readFileSync(testInfo.inputFilePath, "utf8");
+            let expected = readFileSync(testInfo.outputFilePath, "utf8");
+            let parsed = parser(input);
+            let actual = sortExpression(
+              parsed.body[0].declarations[0].init,
+              parsed.comments,
+              input
+            );
+
+            expect(actual).to.equal(expected);
+          });
+        }
+      });
+    });
+  });
+
+  it("Respects a custom order", () => {
+    let input = `let example = 1 | null | undefined`;
+    let expected = `let example = null | undefined | 1`;
+    let parsed = typescriptParse(input);
+    let actual = sortExpression(
+      parsed.body[0].declarations[0].init,
+      parsed.comments,
+      input,
+      {
+        groups: ["null", "undefined"]
+      }
+    );
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("Sets the order if groups is missing from options", () => {
+    let input = `let example = 1 | null | undefined`;
+    let expected = `let example = undefined | null | 1`;
+    let parsed = typescriptParse(input);
+    let actual = sortExpression(
+      parsed.body[0].declarations[0].init,
+      parsed.comments,
+      input,
+      {
+        groups: undefined
+      }
+    );
+
+    expect(actual).to.equal(expected);
+  });
+});

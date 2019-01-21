36fc9d870e4b8346ff20e256ea6b3d468907baa3
Ricky Patel <rickyp@snowcoders.com>
2019-01-20 20:36:31 -0800

    Added code to sort style tag contents

--- src/language-html/reprinter/index.ts
+++ src/language-html/reprinter/index.ts
@@ -1,17 +1,20 @@
 import { parse } from "angular-html-parser";
 import { ILanguage } from "../../language";
+import { CssReprinter } from "../../language-css";
 import { ReprinterOptions } from "../../reprinter-options";
 import { StringUtils } from "../../utilities/string-utils";
 import { sortAttributes } from "../sortAttributes";
 
 export class Reprinter implements ILanguage {
   public static readonly EXTENSIONS = [".html", ".html.txt"];
+  private options: ReprinterOptions;
 
   public getRewrittenContents(
     filename: string,
     fileContents: string,
     options: ReprinterOptions
   ) {
+    this.options = options;
     let ast = parse(fileContents, { canSelfClose: true });
 
     return this.sortNode(
@@ -34,6 +37,24 @@ export class Reprinter implements ILanguage {
         fileContents = this.sortNode(child, fileContents);
       }
     }
+
+    if (node.name === "style") {
+      for (let x = 0; x < node.children.length; x++) {
+        let child = node.children[x];
+        let textBefore = fileContents.substring(
+          0,
+          child.sourceSpan.start.offset
+        );
+        let textAfter = fileContents.substring(child.sourceSpan.end.offset);
+        let newContents = new CssReprinter().getRewrittenContents(
+          "example.css",
+          child.value,
+          this.options
+        );
+        fileContents = textBefore + newContents + textAfter;
+      }
+    }
+
     return fileContents;
   }
 }

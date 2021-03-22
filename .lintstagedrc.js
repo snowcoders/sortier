39d577d9e8c0c9db52e49f1d5d3e72d5ad4412ea
k2snowman69 <rickyp@snowcoders.com>
2021-03-21 19:48:40 -0700

    Updated .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,3 @@
-var configs = require("@snowcoders/renovate-config");
+const configs = require("@snowcoders/renovate-config");
 
-// Since this is sortier, we should run the local version since this package can't depend on itself
-let lintStaged = JSON.stringify(configs.lintStaged);
-lintStaged.replace("sortier", "node bin/index.js");
-
-module.exports = JSON.parse(lintStaged);
+module.exports = configs.lintStaged;

00b064c6f22d4d4f0ec7227dbeab15f7617a4780
k2snowman69 <rickyp@snowcoders.com>
2022-12-03 22:20:46 -0800

    Added common-js dist output

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Added CommonJS output for those who aren't migrated to CJS
 - Breaking
   - Dropped support for node 12 and 14
 
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ import { jest } from "@snowcoders/renovate-config";
 
 export default {
   ...jest,
-  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/dist-cjs/", "<rootDir>/node_modules/"],
 };

2dbe1bdefe00112dd0c0f4660aa0730d7a62d4a4
k2snowman69 <rickyp@snowcoders.com>
2023-05-29 14:49:46 -0700

    Add options selection to playground

--- src-playground/src/components/options-editor/index.tsx
+++ src-playground/src/components/options-editor/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { fileTypes } from "../../utilities/default-text";
+import { Form, InformedProps, Input, SchemaFields, useField } from "informed";
+import { FromSchema } from "json-schema-to-ts";
+import React, { useEffect } from "react";
 import { SortierWorkerInputData } from "../../hooks/use-sortier";
+import { fileTypes } from "../../utilities/default-text";
 import "./styles.css";
 
 export type OptionsEditorProps = {
@@ -8,54 +10,181 @@ export type OptionsEditorProps = {
   fileType: SortierWorkerInputData["type"];
   options: SortierWorkerInputData["options"];
   onFileTypeChange: (fileType: SortierWorkerInputData["type"]) => void;
-  onOptionsChange: (fileType: SortierWorkerInputData["options"]) => void;
+  onOptionsChange: (options: SortierWorkerInputData["options"]) => void;
 };
 
 // TODO: Need to use informed + schema to create the form to edit options
+const schema = {
+  properties: {
+    file: {
+      properties: {
+        type: {
+          oneOf: fileTypes.map((fileType) => ({
+            const: fileType,
+            title: fileType,
+          })),
+          title: "Type",
+          type: "string",
+          "ui:props": {
+            defaultValue: fileTypes[0],
+          },
+        },
+      },
+      title: "File",
+      type: "object",
+    },
+    js: {
+      properties: {
+        sortImportDeclarations: {
+          oneOf: ["first-specifier", "source"].map((value) => ({
+            const: value,
+            title: value,
+          })),
+          title: "sortImportDeclarations",
+          type: "string",
+          "ui:props": {
+            defaultValue: "source",
+          },
+        },
+        // sortImportDeclarationSpecifiers: {
+        //   title: "sortImportDeclarationSpecifiers",
+        //   type: "object",
+        //   properties: {
+        //     groups: {
+        //       title: "groups",
+        //       type: "array",
+        //       "ui:control": "array",
+        //       "ui:props": {
+        //         defaultValue: [{ group: "*" }, { group: "interfaces" }, { group: "types" }],
+        //       },
+        //       items: {
+        //         type: "object",
+        //         required: ["group"],
+        //         properties: {
+        //           group: {
+        //             type: "string",
+        //           },
+        //         },
+        //       },
+        //     },
+        //   },
+        // },
+      },
+      title: "js",
+      type: "object",
+    },
+    // css: {
+    //   title: "css",
+    //   type: "object",
+    //   properties: {
+    // sortDeclarations: {
+    //   title: "sortDeclarations",
+    //   type: "object",
+    //   required: ["overrides"],
+    //   properties: {
+    //     overrides: {
+    //       title: "overrides",
+    //       type: "object",
+    //       properties: {
+    //         groups: {
+    //           title: "groups",
+    //           type: "array",
+    //           "ui:control": "array",
+    //           "ui:props": {
+    //             defaultValue: [
+    //               {
+    //                 group: "top",
+    //               },
+    //               {
+    //                 group: "right",
+    //               },
+    //               {
+    //                 group: "bottom",
+    //               },
+    //               {
+    //                 group: "left",
+    //               },
+    //               {
+    //                 group: "*",
+    //               },
+    //             ],
+    //           },
+    //           items: {
+    //             type: "object",
+    //             required: ["group"],
+    //             properties: {
+    //               group: {
+    //                 type: "string",
+    //               },
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // },
+    //   },
+    // },
+  },
+  required: ["file"],
+  type: "object",
+} as const;
 
+const FieldSet: typeof Input = ({ type, ...props }) => {
+  const field = useField({
+    type: "object",
+    ...props,
+  });
+  const { ref, render, userProps } = field as any;
+  return render(
+    <fieldset ref={ref}>
+      <legend>{userProps.label}</legend>
+      {userProps.children}
+    </fieldset>
+  );
+};
 export function OptionsEditor(props: OptionsEditorProps) {
-  const { className, fileType, onFileTypeChange } = props;
-
-  const onSelectChange: React.DetailedHTMLProps<
-    React.SelectHTMLAttributes<HTMLSelectElement>,
-    HTMLSelectElement
-  >["onChange"] = (e) => {
-    const { value } = e.currentTarget;
-    const validFileType = fileTypes.find((fileType) => fileType === value);
-
-    if (validFileType == null) {
-      return;
-    }
-
-    onFileTypeChange(validFileType);
-  };
+  const formApiRef = React.useRef<any>();
+  const [isFormLoaded, setFormLoaded] = React.useState(false);
+  const { className, fileType, onFileTypeChange, onOptionsChange, options } = props;
 
   //#region callbacks
-  const onSubmit: React.DOMAttributes<HTMLFormElement>["onSubmit"] = (e) => {
-    e.preventDefault();
+  const onSubmit: InformedProps<unknown>["onChange"] = (formState) => {
+    if (!isFormLoaded) {
+      return;
+    }
+    const values = formState.values as FromSchema<typeof schema>;
+
+    const newFileType = fileTypes.find((fileType) => fileType === values.file.type);
+    if (newFileType != null && newFileType !== fileType) {
+      onFileTypeChange(newFileType);
+    }
+    const newOptions = {
+      js: values.js,
+    };
+    onOptionsChange(newOptions);
   };
   //#endregion
 
+  useEffect(() => {
+    formApiRef?.current?.setTheseValues({ file: { type: fileType }, js: options.js });
+    setFormLoaded(true);
+  }, [formApiRef?.current]);
+
+  // TODO: URL doesn't seem to work
+  console.log({ fileType });
   return (
     <section className={"site--options-editor " + className}>
-      <form onSubmit={onSubmit}>
-        <legend>File info</legend>
-        <label htmlFor="filetype">File type</label>
-        <select
-          defaultValue={fileType}
-          name="filetype"
-          onChange={onSelectChange}
-        >
-          {fileTypes.map((value) => (
-            <option key={value} value={value}>
-              {value}
-            </option>
-          ))}
-        </select>
-      </form>
-      <form>
-        <legend>Options</legend>
-      </form>
+      <Form
+        components={{
+          object: FieldSet,
+        }}
+        formApiRef={formApiRef}
+        onChange={onSubmit}
+        schema={schema}
+      >
+        <SchemaFields />
+      </Form>
     </section>
   );
 }

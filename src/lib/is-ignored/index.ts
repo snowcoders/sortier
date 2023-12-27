c44b5faf82dbb1b9459c08855fd8133043de9aca
k2snowman69 <rickyp@snowcoders.com>
2023-12-26 22:37:31 -0800

    Update typescript and tsconfig files

--- src/lib/is-ignored/index.ts
+++ src/lib/is-ignored/index.ts
@@ -4,9 +4,6 @@ export function isIgnored(ignoreFileContents: string, relativeFilePath: string):
   if (ignoreFileContents.length === 0) {
     return false;
   }
-  // @ts-expect-error: ignore seems to be badly typed
-  //  - https://arethetypeswrong.github.io/?p=ignore%405.2.4
-  //  - https://github.com/kaelzhang/node-ignore/issues/96
   const ig = ignore();
   ig.add(ignoreFileContents.split(/\r?\n/));
   return ig.ignores(relativeFilePath);
